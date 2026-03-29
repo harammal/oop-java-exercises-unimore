f25f8e9fdc84da977606a57c9efdabedd000ceaa
harammal <hassanrammal005@proton.me>
2026-03-29 10:29:10 +0200

    Add MatchUp

---
+++ src/main/java/com/harammal/exercises/arrays/MatchUp.java
@@ -0,0 +1,16 @@
+package com.harammal.exercises.arrays;
+
+public class MatchUp {
+
+    public static int matchUp(int[] v1, int[] v2) {
+        int count = 0;
+
+        for (int i = 0; i < v1.length; i++) {
+            if(Math.abs(v1[i] - v2[i]) <= 2 && v1[i] != v2[i]){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
