221731dd5898858510dd80f780269bb848b4481e
harammal <hassanrammal005@gmail.com>
2026-04-02 11:00:50 +0200

    Add RemoveFirstTwoCharsIf

---
+++ src/main/java/com/harammal/exercises/strings/RemoveFirstTwoCharsIf.java
@@ -0,0 +1,33 @@
+package com.harammal.exercises.strings;
+
+public class RemoveFirstTwoCharsIf {
+
+    public static String removeFirstTwoCharsIfCharAt(String string) {
+        StringBuilder sb = new StringBuilder();
+
+        if (!string.isEmpty() && string.charAt(0) == 'H') {
+            sb.append('H');
+        }
+        if (string.length() >= 2 && string.charAt(1) == 'e') {
+            sb.append('e');
+        }
+        if (string.length() >= 3) {
+            sb.append(string.substring(2));
+        }
+
+        return sb.toString();
+    }
+
+    public static String removeFirstTwoCharsIfStringBuilder(String string) {
+        StringBuilder sb = new StringBuilder(string);
+
+        if (string.length() >= 2 && string.charAt(1) != 'e') {
+            sb.deleteCharAt(1);
+        }
+        if (!string.isEmpty() && string.charAt(0) != 'H') {
+            sb.deleteCharAt(0);
+        }
+
+        return sb.toString();
+    }
+}
