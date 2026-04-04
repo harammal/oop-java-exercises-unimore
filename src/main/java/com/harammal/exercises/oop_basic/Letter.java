b341274c388ff1e14f06ab348bc1713144bde3d7
harammal <hassanrammal005@proton.me>
2026-04-04 11:54:34 +0200

    Add Letter

---
+++ src/main/java/com/harammal/exercises/oop_basic/Letter.java
@@ -0,0 +1,42 @@
+package com.harammal.exercises.oop_basic;
+
+/**
+ * A letter written in a standard format.
+ * Its methods allow for setting sender, recipient and the lines of the body of the letter.
+ * Once all the components have been set, the getText() methods returns the letter in a standard format.
+ */
+public class Letter {
+    private final String from;
+    private final String to;
+    private StringBuilder lines;
+
+    /**
+     * Constructs and initializes a letter with a sender and a recipient
+     *
+     * @param from the sender
+     * @param to   the recipient
+     */
+    public Letter(String from, String to) {
+        this.from = from;
+        this.to = to;
+        this.lines = new StringBuilder();
+    }
+
+    /**
+     * Adds a line of text to the body of the letter
+     *
+     * @param line the line to be added
+     */
+    public void addLine(String line) {
+        lines.append("\n" + line);
+    }
+
+    /**
+     * Returns a representation of the complete letter
+     *
+     * @return the complete letter
+     */
+    public String getText() {
+        return "Dear " + to + ":\n" + lines.toString() + "\n\nSincerely,\n\n" + from;
+    }
+}
