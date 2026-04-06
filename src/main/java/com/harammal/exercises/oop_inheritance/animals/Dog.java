a105484f562144bf8d36cf80053a0df48c52a2df
harammal <hassanrammal005@gmail.com>
2026-04-06 20:09:15 +0200

    Add animals

---
+++ src/main/java/com/harammal/exercises/oop_inheritance/animals/Dog.java
@@ -0,0 +1,13 @@
+package com.harammal.exercises.oop_inheritance.animals;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public String makeSound() {
+        return "woof";
+    }
+}
