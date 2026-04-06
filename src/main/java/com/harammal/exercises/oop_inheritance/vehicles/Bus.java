0ef6d1afe2f85ab5dc17575cb2dd59a0db20c0b0
harammal <hassanrammal005@gmail.com>
2026-04-06 20:11:19 +0200

    Add vehicles

---
+++ src/main/java/com/harammal/exercises/oop_inheritance/vehicles/Bus.java
@@ -0,0 +1,15 @@
+package com.harammal.exercises.oop_inheritance.vehicles;
+
+public class Bus extends Transport {
+    private boolean hasDoubleDeck;
+
+    public Bus(String name, int maxPassengers, boolean hasDoubleDeck) {
+        super(name, maxPassengers);
+        this.hasDoubleDeck = hasDoubleDeck;
+    }
+
+    @Override
+    public String getCapacityInfo() {
+        return "Bus{" + "name='" + name + "', maxPassengers=" + maxPassengers + ", doubleDeck=" + hasDoubleDeck + "}";
+    }
+}
