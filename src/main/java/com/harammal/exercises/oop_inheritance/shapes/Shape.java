1809df030d3c9371f2020f3cad381bb0fcad0db9
harammal <hassanrammal005@gmail.com>
2026-04-06 20:11:00 +0200

    Add shapes

---
+++ src/main/java/com/harammal/exercises/oop_inheritance/shapes/Shape.java
@@ -0,0 +1,26 @@
+package com.harammal.exercises.oop_inheritance.shapes;
+
+public class Shape {
+    protected String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getArea() {
+        return 0;
+    }
+
+    public String getDescription() {
+        return "Shape{" + "color='" + color + "'}";
+    }
+
+    @Override
+    public String toString() {
+        return getDescription();
+    }
+}
