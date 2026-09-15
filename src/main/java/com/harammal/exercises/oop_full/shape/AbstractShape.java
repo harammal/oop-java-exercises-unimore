723d78554befda3d0225ce2a3514ac36d939ee94
harammal <hassanrammal005@proton.me>
2026-09-15 17:36:37 +0200

    Add AbstractShape

---
+++ src/main/java/com/harammal/exercises/oop_full/shape/AbstractShape.java
@@ -0,0 +1,30 @@
+package com.harammal.exercises.oop_full.shape;
+
+public abstract class AbstractShape implements Computable, Movable, Resizable{
+    protected String id;
+    protected String color;
+
+    public AbstractShape(String id, String color) {
+        this.id = id;
+        this.color = color;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (!color.matches("^#[0-9a-fA-F]{6}")) {
+            throw new IllegalArgumentException("Invalid RGB Color (#RRGGBB)");
+        }
+        this.color = color;
+    }
+}
